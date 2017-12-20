77e2be2a1531965e3a1a20f16b9a9cc04ed5f44c
Ruha Peera <ruha.rizwan@gmail.com>
2017-12-20 10:40:19 -0500

    linked files to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,22 @@
 //Getting the document ready
 $(document).ready(function() {
 
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB44JcP2uAlCtSsazMZql0TmOotmwnGcIo",
+	authDomain: "train-scheduler-995b7.firebaseapp.com",
+	databaseURL: "https://train-scheduler-995b7.firebaseio.com",
+	projectId: "train-scheduler-995b7",
+	storageBucket: "train-scheduler-995b7.appspot.com",
+	messagingSenderId: "657247607969"
+	};
+
+firebase.initializeApp(config);
+
+//creating a variable for database
+var database = firebase.database();
+
+
 
 
 });//closing document.ready function
